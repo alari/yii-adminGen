a16a0449252fd68139709c9c3af89468d207731c
alari <name.alari@gmail.com>
2012-09-04 13:26:12 +0400

    default visibility

--- AdminGenModule.php
+++ AdminGenModule.php
@@ -21,40 +21,46 @@ class AdminGenModule extends CWebModule
         foreach (Yii::app()->getModules() as $name => $conf) {
             $module = Yii::app()->getModule($name);
             $addLinks = method_exists($module, "adminGenLinks") ? $module->adminGenLinks() : $this->getModuleAdminLinks($name);
-            if(is_array($addLinks) && count($addLinks)) {
-                if(!isset($addLinks[0]) || !is_array($addLinks[0])) {
+            if (is_array($addLinks) && count($addLinks)) {
+                if (!isset($addLinks[0]) || !is_array($addLinks[0])) {
                     $addLinks = array($addLinks);
                 }
-                foreach($addLinks as $l) $links[] = $l;
+                foreach ($addLinks as $l) {
+                    if (!array_key_exists("visible", $l)) {
+                        $l["visible"] = !Yii::app()->user->isGuest;
+                    }
+                    $links[] = $l;
+                }
             }
         }
 
         // Controller admin links
-        if($this->addControllerMenu) {
+        if ($this->addControllerMenu) {
             $addLinks = Yii::app()->getController()->menu;
-            if(is_array($addLinks) && count($addLinks)) {
-                if(!isset($addLinks[0]) || !is_array($addLinks[0])) {
+            if (is_array($addLinks) && count($addLinks)) {
+                if (!isset($addLinks[0]) || !is_array($addLinks[0])) {
                     $addLinks = array($addLinks);
                 }
-                foreach($addLinks as $l) $links[] = $l;
+                foreach ($addLinks as $l) $links[] = $l;
             }
         }
 
         return $links;
     }
 
-    public function getModuleAdminLinks($name) {
+    public function getModuleAdminLinks($name)
+    {
         static $moduleLinks = null;
-        if(!$moduleLinks) {
+        if (!$moduleLinks) {
             $moduleLinks = array(
-                "user"=>array(
+                "user" => array(
                     array('url' => Yii::app()->getModule('user')->profileUrl, 'label' => Yii::app()->getModule('user')->t("Profile"), 'visible' => !Yii::app()->user->isGuest),
                     array('url' => Yii::app()->getModule('user')->logoutUrl, 'label' => Yii::app()->getModule('user')->t("Logout") . ' (' . Yii::app()->user->name . ')', 'visible' => !Yii::app()->user->isGuest),
                 )
             );
         }
 
-        if(array_key_exists($name, $moduleLinks)) {
+        if (array_key_exists($name, $moduleLinks)) {
             return $moduleLinks[$name];
         }
         return null;
