c7be1cd4de769f42387e34825a5616dae06ce8b0
Egorvah <scsware@yandex.ru>
2012-09-21 11:24:36 +0400

    menu

--- views/adminGen/index.php
+++ views/adminGen/index.php
@@ -9,6 +9,15 @@ $focus = function($l){
 };
 
 $links = Yii::app()->getModule("adminGen")->getLinks();
+
+$columnLinks = array();
+$columns = 3;
+$i=0;
+foreach ($links as $l) {
+    $i++;
+    $columnLinks[$i][]= $l;
+    if ($i == $columns) $i=0;
+}
 ?>
 
 <h1>Администрирование сайта</h1>
@@ -31,14 +40,17 @@ $links = Yii::app()->getModule("adminGen")->getLinks();
 <?}?>
 
 <h4>Все действия</h4>
-<div class="row">
-    <?foreach ($links as $l) {
-    echo "<div class='span4'>";
-    echo "<strong>" . CHtml::link($l["label"], $l["url"] ? : "#") . "</strong><br/>";
-    if (isset($l["items"])) $this->widget('zii.widgets.CMenu', array(
-        'items' => $l["items"],
-        'htmlOptions' => array('class' => 'nav nav-tabs nav-stacked'),
-    ));
-    echo "</div>";
-}?>
-</div>
+
+<?foreach ($columnLinks as $colinmn) {?>
+    <div class="span4 m-nomargin">
+        <?foreach ($colinmn as $l) {
+            echo "<div class='span12'>";
+            echo "<strong>" . CHtml::link($l["label"], $l["url"] ? : "#") . "</strong><br/>";
+            if (isset($l["items"])) $this->widget('zii.widgets.CMenu', array(
+                'items' => $l["items"],
+                'htmlOptions' => array('class' => 'nav nav-tabs nav-stacked'),
+            ));
+            echo "</div>";
+        }?>
+    </div>
+<? }?>
